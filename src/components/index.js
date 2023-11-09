b21a1c3d57287c1ba0f33d3a0fc6af456b0eb979
AdeshDaniel <stockhutfxcompany@gmail.com>
2023-11-09 02:08:20 +0100

    components and assets

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Loader from "./Loader";
+import Contact from "./Contact";
+import Works from "./Works";
+import Experience from "./Experience";
+import Feedbacks from "./Feedbacks";
+import Tech from "./Tech";
+import About from "./About";
+
+export {
+  Navbar,
+  Hero,
+  Loader,
+  Contact,
+  Works,
+  Experience,
+  Feedbacks,
+  Tech,
+  About,
+};
