20cbf4821d5cf0c05bd23c627e385f86dc1f8d9d
AdeshDaniel <stockhutfxcompany@gmail.com>
2023-11-09 02:41:16 +0100

    made utils and others

---
+++ src/components/canvas/index.js
@@ -0,0 +1,6 @@
+import BallCanvas from "./BallCanvas";
+import ComputersCanvas from "./ComputersCanvas";
+import EarthCanvas from "./EarthCanvas";
+import StarsCanvas from "./StarsCanvas";
+
+export { BallCanvas, ComputersCanvas, EarthCanvas, StarsCanvas };
--- src/components/index.js
+++ src/components/index.js
@@ -1,21 +1,31 @@
-import Navbar from "./Navbar";
+import {
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+} from "./canvas";
 import Hero from "./Hero";
-import Loader from "./Loader";
-import Contact from "./Contact";
-import Works from "./Works";
-import Experience from "./Experience";
-import Feedbacks from "./Feedbacks";
-import Tech from "./Tech";
+import Navbar from "./Navbar";
 import About from "./About";
+import Tech from "./Tech";
+import Experience from "./Experience";
+import Works from "./Works";
+import Feedbacks from "./Feedbacks";
+import Contact from "./Contact";
+import CanvasLoader from "./Loader";
 
 export {
-  Navbar,
   Hero,
-  Loader,
-  Contact,
-  Works,
-  Experience,
-  Feedbacks,
-  Tech,
+  Navbar,
   About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  CanvasLoader,
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
 };
